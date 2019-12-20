1663dcf51537d014ef10f84b7835b95d2b7a1a77
Tran Ngoc <hocmai6@gmail.com>
2019-12-20 23:18:41 +0700

    Update ProfileFragment.java
    - Get User info fill profile

--- TravelApp/app/src/main/java/com/kumeo/traveltour/view/Fragment/ProfileFragment.java
+++ TravelApp/app/src/main/java/com/kumeo/traveltour/view/Fragment/ProfileFragment.java
@@ -1,16 +1,30 @@
 package com.kumeo.traveltour.view.Fragment;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kumeo.traveltour.R;
+import com.kumeo.traveltour.UserInfo;
+import com.kumeo.traveltour.extras.MyApplication;
+import com.kumeo.traveltour.retrofit.Service.User.UserAPI;
+import com.kumeo.traveltour.retrofit.retrofitRequest;
+import com.kumeo.traveltour.view.Activity.SplashActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -68,8 +82,54 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        UserAPI user;
+        String header =  SplashActivity.appPreference.getToken();
+        user = retrofitRequest.getRetrofitInstance().create(UserAPI.class);
+        Call<UserInfo> info = user.myInfo(header);
+        info.enqueue(new Callback<UserInfo>() {
+            @Override
+            public void onResponse(Call<UserInfo> call, Response<UserInfo> response) {
+                if (response.isSuccessful()){
+                    TextView fullName = view.findViewById(R.id.txtFullName);
+                    TextView email = view.findViewById(R.id.emailPro);
+                    TextView phone = view.findViewById(R.id.phonePro);
+                    TextView address = view.findViewById(R.id.addPro);
+                    TextView gender = view.findViewById(R.id.genPro);
+                    TextView dob = view.findViewById(R.id.dobPro);
+
+                    fullName.setText(""+response.body().getFullName());
+                    email.setText("Email : "+response.body().getEmail());
+                    phone.setText("SDT : "+response.body().getPhone());
+                    address.setText("Địa chỉ : "+response.body().getAddress());
+                    if (response.body().getGender()!=1)
+                    {
+                        gender.setText("Giới tính : Nam");
+                    }
+                    else {
+                        gender.setText("Giới tính : Nữ");}
+                    dob.setText("Ngày Sinh : "+response.body().getDob());
+
+                    if (response.body().getFullName()== null) fullName.setText("[Chưa cập nhật]");
+                    if (response.body().getAddress()== null) address.setText("Địa chỉ : [Chưa cập nhật]");
+                    if (response.body().getDob()== null) dob.setText("Ngày Sinh : [Chưa cập nhật]");
+                    if (response.body().getEmail()== null) email.setText("Email : [Chưa cập nhật]");
+                    if (response.body().getPhone()== null) phone.setText("SDT : [Chưa cập nhật]");
+                }
+            }
+            @Override
+            public void onFailure(Call<UserInfo> call, Throwable t) {
+            }
+        });
         //initialization(view);
         //getMyTrips();
+        Button signOutBtn = view.findViewById(R.id.signOutBtn);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SplashActivity.appPreference.showToast("Signout Successful");
+            }
+        });
         return view;
     }
 
