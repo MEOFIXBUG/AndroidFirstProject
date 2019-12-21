0a3cdee73feb4a68399f003d4bd38773f47b83d1
Tran Ngoc <hocmai6@gmail.com>
2019-12-21 22:51:16 +0700

    update UserAPI and add active email API
    - add send request active email api
    - add active email api
    - add class for active email api call

---
+++ TravelApp/app/src/main/java/com/kumeo/traveltour/response/ActiveResultResponse.java
@@ -0,0 +1,20 @@
+package com.kumeo.traveltour.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ActiveResultResponse {
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+
+}
--- TravelApp/app/src/main/java/com/kumeo/traveltour/retrofit/Service/User/UserAPI.java
+++ TravelApp/app/src/main/java/com/kumeo/traveltour/retrofit/Service/User/UserAPI.java
@@ -6,19 +6,24 @@ import android.content.SharedPreferences;
 import com.kumeo.traveltour.R;
 import com.kumeo.traveltour.UserInfo;
 import com.kumeo.traveltour.extras.MyApplication;
+import com.kumeo.traveltour.response.ActiveResponse;
+import com.kumeo.traveltour.response.ActiveResultResponse;
 
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 public interface UserAPI {
 
  @GET("/user/info")
  Call<UserInfo> myInfo(@Header("Authorization") String author);
- /*@Headers({
-         "Accept: application/json",
-         "Content-Type: application/json"
- })*/
+ @GET("/user/send-active")
+ Call <ActiveResponse> sendActive(@Query("userId") Integer userID,@Query("type") String type );
+ @GET("/user/active")
+ Call <ActiveResultResponse> getActiveStatus(@Query("userId") Integer userID, @Query("type") String type, @Query("verifyCode") String verifyCode);
 
 }
