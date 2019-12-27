4ebec296b65645278e128c928170fbd8a0014b85
MEOFIXBUG <57151978+MEOFIXBUG@users.noreply.github.com>
2019-12-27 13:54:11 +0700

    fragment to update tour info, invite user, make pubic ,user viewmodel, search+ autocomplete

--- TravelApp/app/src/main/java/com/ygaps/travelapp/response/UserListRp.java
+++ TravelApp/app/src/main/java/com/ygaps/travelapp/response/UserListRp.java
@@ -1,4 +1,33 @@
 package com.ygaps.travelapp.response;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.ygaps.travelapp.UserInfo;
+import com.ygaps.travelapp.model.Tour;
+
+import java.util.List;
+
 public class UserListRp {
+    @SerializedName("total")
+    @Expose
+    private long total;
+    @SerializedName("users")
+    @Expose
+    private List<UserInfo> users=null;
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer totalResults) {
+        this.total = totalResults;
+    }
+
+    public List<UserInfo> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserInfo> data) {
+        this.users = data;
+    }
 }
