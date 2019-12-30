3e7f2d90da59acb0266488f059f20e08a651b2e0
Tran Ngoc <hocmai6@gmail.com>
2019-12-30 23:48:04 +0700

    Đừng xóa sửa lung tung

--- TravelApp/app/src/main/java/com/ygaps/travelapp/MyFirebaseService.java
+++ TravelApp/app/src/main/java/com/ygaps/travelapp/MyFirebaseService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
@@ -19,15 +20,23 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.ygaps.travelapp.R;
+import com.ygaps.travelapp.model.isAccept;
+import com.ygaps.travelapp.response.RecoveryResponse;
+import com.ygaps.travelapp.retrofit.Service.Tour.TourAPI;
+import com.ygaps.travelapp.retrofit.retrofitRequest;
 import com.ygaps.travelapp.view.Activity.MainActivity;
 import com.ygaps.travelapp.view.Activity.SplashActivity;
 import com.ygaps.travelapp.view.Fragment.ProfileFragment;
 
 import java.util.Map;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MyFirebaseService extends FirebaseMessagingService {
     private static final String TAG = "Firebaselog";
-
+    private static NotificationManager notificationManager;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // handle a notification payload.
@@ -43,7 +52,68 @@ public class MyFirebaseService extends FirebaseMessagingService {
 
         sendRegistrationToServer(token);
     }
+    public static class DenyAction extends BroadcastReceiver {
 
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Myapp","Deny");
+            Integer tourID = intent.getExtras().getInt("TourID");
+            isAccept param = new isAccept();
+            param.setAccepted(false);
+            param.setTourId(tourID.toString());
+            String header = SplashActivity.appPreference.getToken();
+            TourAPI tour = retrofitRequest.getRetrofitInstance().create(TourAPI.class);
+            Call<RecoveryResponse> tourCall = tour.responseTourInvite(header,param);
+            tourCall.enqueue(new Callback<RecoveryResponse>() {
+                @Override
+                public void onResponse(Call<RecoveryResponse> call, Response<RecoveryResponse> response) {
+                    if (response.isSuccessful()) {
+                        if (response.code() == 200) {
+//                            SplashActivity.appPreference.showToast("Deny invite of tour ID : "+ tourID.toString());
+                            SplashActivity.appPreference.showToast("Deny invite successful");
+                            notificationManager.cancel(tourID);
+                        } else {
+                            SplashActivity.appPreference.showToast("Failed");
+                        }
+                    }
+                }
+                @Override
+                public void onFailure(Call<RecoveryResponse> call, Throwable t) {
+                }
+            });
+
+        }
+    }
+    public static class ViewAction extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Integer tourID = intent.getExtras().getInt("TourID");
+            isAccept param = new isAccept();
+            param.setAccepted(true);
+            param.setTourId(tourID.toString());
+            String header = SplashActivity.appPreference.getToken();
+            TourAPI tour = retrofitRequest.getRetrofitInstance().create(TourAPI.class);
+            Call<RecoveryResponse> tourCall = tour.responseTourInvite(header,param);
+            tourCall.enqueue(new Callback<RecoveryResponse>() {
+                @Override
+                public void onResponse(Call<RecoveryResponse> call, Response<RecoveryResponse> response) {
+                    if (response.isSuccessful()) {
+                        if (response.code() == 200) {
+//                            SplashActivity.appPreference.showToast("Deny invite of tour ID : "+ tourID.toString());
+                            SplashActivity.appPreference.showToast("Accept invite successful");
+                            notificationManager.cancel(tourID);
+                        } else {
+                            SplashActivity.appPreference.showToast("Failed");
+                        }
+                    }
+                }
+                @Override
+                public void onFailure(Call<RecoveryResponse> call, Throwable t) {
+                }
+            });
+        }
+    }
     private void sendRegistrationToServer(String token) {
         // TODO: Implement this method to send token to your app server.
     }
@@ -55,6 +125,21 @@ public class MyFirebaseService extends FirebaseMessagingService {
 
         //Custom
         RemoteViews myNoti = new RemoteViews(getPackageName(), R.layout.notification);
+        int m = Integer.parseInt(data.get("id"));
+        //Deny
+        Intent closeButton = new Intent("Deny_action");
+        closeButton.putExtra("TourID", m);
+        closeButton.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent closeIntent = PendingIntent.getBroadcast(this, 0, closeButton, PendingIntent.FLAG_CANCEL_CURRENT);
+        myNoti.setOnClickPendingIntent(R.id.btnDenied, closeIntent);
+        //Acccept
+        Intent acceptButton = new Intent("View_action");
+        acceptButton.putExtra("TourID", m);
+        acceptButton.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent acceptIntent = PendingIntent.getBroadcast(this, 0, acceptButton, PendingIntent.FLAG_CANCEL_CURRENT);
+        myNoti.setOnClickPendingIntent(R.id.btnAccept, acceptIntent);
+
+
         String channelId = getString(R.string.app_name);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         String tourName = "A user invited you to tour : "+ data.get("name");
@@ -69,7 +154,7 @@ public class MyFirebaseService extends FirebaseMessagingService {
                         .setContentIntent(pendingIntent)
                         .setPriority(NotificationManager.IMPORTANCE_HIGH);
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Since android Oreo notification channel is needed.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -81,6 +166,6 @@ public class MyFirebaseService extends FirebaseMessagingService {
             notificationManager.createNotificationChannel(channel);
         }
 
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(m, notificationBuilder.build());
     }
 }
--- TravelApp/app/src/main/java/com/ygaps/travelapp/retrofit/Service/Tour/TourAPI.java
+++ TravelApp/app/src/main/java/com/ygaps/travelapp/retrofit/Service/Tour/TourAPI.java
@@ -7,7 +7,9 @@ import com.ygaps.travelapp.model.StopPoint;
 import com.ygaps.travelapp.model.StopPointsOfTour;
 import com.ygaps.travelapp.model.Tour;
 import com.ygaps.travelapp.model.coordRequest;
+import com.ygaps.travelapp.model.isAccept;
 import com.ygaps.travelapp.model.toInvited;
+import com.ygaps.travelapp.response.RecoveryResponse;
 import com.ygaps.travelapp.response.ReviewTourResponse;
 import com.ygaps.travelapp.response.ServiceResponse;
 import com.ygaps.travelapp.response.StatusResponse;
@@ -20,10 +22,14 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface TourAPI {
+    @POST("/tour/response/invitation")
+    Call <RecoveryResponse> responseTourInvite(@Header("Authorization") String author, @Body isAccept param);
+
     @GET("tour/list")
     Call<TourResponse> getListTour(@Query("rowPerPage") long perpage ,
                                    @Query("pageNum") long page,
